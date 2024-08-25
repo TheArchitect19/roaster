43bf0b13f5241ab3fff2e0e1c368ff86bfd68bda
Ayush Kumar <ayush.krsh9@gmail.com>
2024-08-25 14:23:23 +0530

    add cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { FileUploadDemo } from "./components/upload";
+import { InfiniteMovingCardsDemo } from "./components/cards";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <FileUploadDemo />
+      <InfiniteMovingCardsDemo />
     </main>
   );
 }
